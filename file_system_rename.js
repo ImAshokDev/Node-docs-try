cf9af76bb3e787f6abdfa6a5dcd6756c6286944f
krist yohan <massashok420@gmail.com>
2019-11-30 17:51:48 +0530

    fs rename added

---
+++ file_system_rename.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+// creating a file using write method
+fs.writeFile('khrist.txt', 'Hello khrist', function(err) {
+  if (err) throw err;
+  console.log('File created using write method');
+});
+
+// renamethe above created file
+fs.rename('khrist.txt', 'yohanRename.txt', function(err) {
+  if (err) throw err;
+  console.log('Deleted the file created above');
+});
