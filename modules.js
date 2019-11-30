816ef20c39390bc6bfeb1231cb0f12e92863846e
krist yohan <massashok420@gmail.com>
2019-11-30 15:39:22 +0530

    modules

---
+++ modules.js
@@ -0,0 +1,15 @@
+// Modules as Js libraries
+
+var http = require('http');
+var khristModule = require('./khristfirstmodule');
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(
+      'Current Data and Time using khristModule: ' +
+        khristModule.KhristDateTime()
+    );
+    res.end();
+  })
+  .listen(8080);
