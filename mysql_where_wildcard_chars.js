b713765459dcd30fcfef85810ab952c02f95dd6e
krist yohan <massashok420@gmail.com>
2019-12-02 13:33:31 +0530

    where wildcard char

---
+++ mysql_where_wildcard_chars.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'khristyohan',
+  password: 'rootmysql',
+  database: 'khristdb'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected to the database');
+  var sql = "SELECT * FROM developer WHERE designation LIKE 'C%'";
+  connection.query(sql, function(err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
